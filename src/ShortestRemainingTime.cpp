1c7effa3dab067ee973347db324bcff0101e78a7
Mitesh Kumar <miteshkumarca@gmail.com>
2021-04-01 01:15:33 -0400

    Update report

--- src/ShortestRemainingTime.cpp
+++ src/ShortestRemainingTime.cpp
@@ -4,10 +4,8 @@
  *
  *  Simulation Project - ShortestRemainingTime.cpp
  *
- *  Authors:
+ *  Author:
  *    Mitesh Kumar  [ kumarm4 ]
- *    Jason Lam     [  lamj7  ]
- *    William He    [  hew7   ]
  *
  */
 
